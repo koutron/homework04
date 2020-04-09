bda85f14ace7a6303df7ad6d73aeb12e4299573c
Kouros <kamousavi@gmail.com>
2020-04-09 10:05:51 -0400

    initial commit.  scaffolding in place.

---
+++ script.js
@@ -0,0 +1,41 @@
+var data = [
+    {
+        ques: "What is Kouros's favorite color?", ans: [{ option: "blue", isCorrect: false }, { option: "green", isCorrect: false }, { option: "red", isCorrect: false }, { option: "black", isCorrect: true }]
+    },
+    {
+        ques: "What is Kouros's favorite website?", ans: [{ option: "Google", isCorrect: false }, { option: "Yahoo", isCorrect: false }, { option: "Microsoft", isCorrect: false }, { option: "Bing", isCorrect: true }]
+    },
+    {
+        ques: "What is Kouros's favorite food?", ans: [{ option: "Burgers", isCorrect: true }, { option: "Tacos", isCorrect: false }, { option: "Pizza", isCorrect: false }, { option: "Sushi", isCorrect: false }]
+    }
+]
+
+
+document.getElementById('begin').addEventListener('click', function () {
+    document.getElementById('welcome').setAttribute('style', 'display: none');
+    document.getElementById('begin').setAttribute('style', 'display: none');
+    populateQandA(0);
+    
+});
+
+function populateQandA(idx) {
+    document.getElementById("question").textContent = data[idx].ques;
+    document.getElementById("a0").textContent = data[idx].ans[0].option;
+    document.getElementById("a1").textContent = data[idx].ans[1].option;
+    document.getElementById("a2").textContent = data[idx].ans[2].option;
+    document.getElementById("a3").textContent = data[idx].ans[3].option;
+    isAnswerCorrect(idx);
+}
+
+function isAnswerCorrect(idx) {
+    document.getElementById("answersList").addEventListener("click", function (event) {
+        event.preventDefault();
+        if (data[idx].ans[event.target.value].isCorrect === true) {
+            alert('Correct!');
+        } else {
+            alert('WRONG!');
+        }
+        populateQandA(idx+1);
+
+    });
+}
